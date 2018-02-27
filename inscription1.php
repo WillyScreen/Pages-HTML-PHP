4a170cd986b645852211b81427f9a98790d65247
Capgir <36543767+Capgir@users.noreply.github.com>
2018-02-27 14:03:59 +0100

    Add files via upload

---
+++ inscription1.php
@@ -0,0 +1,17 @@
+<?php
+// on teste si le visiteur a soumis le formulaire
+if (isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription') {
+	// on teste l'existence de nos variables. On teste également si elles ne sont pas vides
+	if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pass']) && !empty($_POST['pass'])) && (isset($_POST['pass_confirm']) && !empty($_POST['pass_confirm']))) {
+	// on teste les deux mots de passe
+	if ($_POST['pass'] != $_POST['pass_confirm']) {
+		$erreur = 'Les 2 mots de passe sont différents.';
+	}
+	else {
+		$bdd = new PDO('mysql:host=localhost;dbname=user;charset=utf8', 'root', '');
+	}
+	catch (Exception $e)
+	{
+        	die('Erreur : ' . $e->getMessage());
+	}
+?>
